b25a13ee72ae9dea7963074cb5d15e1732a24705
ishan10902 <ishansugath@gmail.com>
2025-07-15 23:48:44 +0530

    complete

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -37,7 +37,7 @@ const View = () => {
                                                 <div class="card-body">
                                                     <h5 class="card-title">{value.product}</h5>
                                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                    <a href="#" class="btn btn-primary">Buy Now</a>
                                                 </div>
                                             </div>
                                         </div>
